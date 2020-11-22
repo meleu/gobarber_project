c071daf367a99750924102d612e42e02225ecf90
meleu <meleuzord@yahoo.com>
2020-11-21 23:28:57 -0300

    kickstarting gobarber

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'hello world!' }));
+
+app.listen(3333, () => console.log('backend has been started!'));
