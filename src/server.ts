cc3758092027820d04efd4993d1b2ba8f4cda4d0
meleu <meleuzord@yahoo.com>
2020-11-22 01:13:50 -0300

    coding style

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'hello world!' }));
+app.get('/', (request, response) => {
+  response.json({ message: 'hello world!' });
+});
 
 app.listen(3333, () => console.log('backend has been started!'));
