00d1a9a3b75fc1bbf84ee65ed794936078fbf9ce
meleu <meleuzord@yahoo.com>
2020-11-23 07:46:44 -0300

    migration add email to user's table

---
+++ src/database/migrations/1606126305804-AddEmailToUsersTable.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddEmailToUsersTable1606126305804
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'email');
+  }
+}
