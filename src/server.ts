fa70a999c889691c8a593a9f669f6ea1d3e2b425
meleu <meleuzord@yahoo.com>
2020-11-23 15:45:46 -0300

    serve static files

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import './database';
 import routes from './routes';
 import AppError from './errors/AppError';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
