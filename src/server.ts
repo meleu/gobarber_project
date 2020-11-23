90447fff7e481020fbc87900363d6a20924c9517
meleu <meleuzord@yahoo.com>
2020-11-23 05:46:13 -0300

    error handler

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import 'reflect-metadata';
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 
 import './database';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('backend has been started!'));
